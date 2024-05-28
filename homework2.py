89f06fa0e1fa4b0ce8afda1e5da4911ef6689741
MisterCbing <150890001+MisterCbing@users.noreply.github.com>
2024-05-28 10:02:16 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+dz_quantity = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / dz_quantity
+print('Курс:', course_name + ',', 'всего задач:',str(dz_quantity) + ',', 'затрачено часов:', str(hours_spent)+',', 'среднее время выполнения', time_per_task, 'часа.')
