48824fba88695de51a071597b4b92d39670f479b
MisterCbing <150890001+MisterCbing@users.noreply.github.com>
2024-05-28 14:00:01 +0400

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 2, 3, 4, 5,'вышел', 'зайчик', 'погулять')
+print(immutable_var)
+#immutable_var[0] = 2
+
+mutable_list = ['вдруг', 'охотник', 'выбегает', 5, 4, 3, 2, 1]
+mutable_list[3:] = ['прямо', 'в зайчика', 'стреляет']
+print(mutable_list)
